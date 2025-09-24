fa1460bf0999b7a7be329e23ea71b6d8edbb51dc
Matsuel <matheolang@icloud.com>
2025-09-24 21:14:10 +0200

    Initial Commit

---
+++ TestSwift/TestSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftApp.swift
+//  TestSwift
+//
+//  Created by Matheo Lang on 24/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct TestSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestSwiftTests/TestSwiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftTests.swift
+//  TestSwiftTests
+//
+//  Created by Matheo Lang on 24/09/2025.
+//
+
+import Testing
+@testable import TestSwift
+
+struct TestSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
